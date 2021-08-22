fb6aa4f38032fa49ededd0028acc1521d465c7ad
Lily Finley <TechnoFairyGirl@users.noreply.github.com>
2021-08-22 04:10:18 -0500

    Initial code commit.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace UpdatePfSenseCerts
+{
+	class Config
+	{
+		public string PfApiKey { get; set; }
+		public string PfApiSecret { get; set; }
+		public string PfUrl { get; set; }
+		public string CertFile { get; set; }
+		public string KeyFile { get; set; }
+	}
+
+	static class Program
+	{
+		static Config config;
+
+		static JObject PfApiCall(string query, string postData = null)
+		{
+			var timestamp = DateTime.UtcNow.ToString("yyyyMMddZHHmmss");
+			var nonce = Util.Random(4).ToHexString();
+			var hash = Util.Sha256($"{config.PfApiSecret}{timestamp}{nonce}".GetBytes()).ToHexString();
+			var auth = $"{config.PfApiKey}:{timestamp}:{nonce}:{hash}";
+
+			using var http = new WebClient();
+			var url = $"{config.PfUrl}/fauxapi/v1/?{query}";
+			http.Headers.Add("fauxapi-auth", auth);
+			var response = postData != null ? http.UploadString(url, postData) : http.DownloadString(url);
+
+			var json = JObject.Parse(response);
+
+			return json;
+		}
+
+		static void Main(string[] args)
+		{
+			var exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+			var configPath =
+				args.Length >= 1 ? args[0] :
+				Path.Combine(Path.GetDirectoryName(exePath), "config.json");
+			config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath));
+
+			Console.WriteLine($"Updating SSL certificates on '{config.PfUrl}'.");
+			Console.WriteLine($"Cert file: {config.CertFile}");
+			Console.WriteLine($"Key file: {config.KeyFile}");
+
+			ServicePointManager.ServerCertificateValidationCallback =
+				(sender, certificate, chain, sslPolicyErrors) => true;
+
+			var certFile = File.ReadAllBytes(config.CertFile);
+			var keyFile = File.ReadAllBytes(config.KeyFile);
+
+			var configGet = PfApiCall("action=config_get");
+
+			Console.WriteLine($"Got config. ({configGet["action"]}:{configGet["message"]})");
+
+			var certRef = (string)configGet["data"]["config"]["system"]["webgui"]["ssl-certref"];
+			var cert = configGet["data"]["config"]["cert"].First(cert => (string)cert["refid"] == certRef);
+			cert["crt"] = certFile.GetBase64();
+			cert["prv"] = keyFile.GetBase64();
+			var certConfig = "{" + cert.Parent.Parent.ToString() + "}";
+
+			var updateResult = PfApiCall("action=config_patch", certConfig);
+
+			Console.WriteLine($"Updated config. ({updateResult["action"]}:{updateResult["message"]})");
+
+			var restartResult = PfApiCall("action=send_event", "[\"service restart webgui\"]");
+
+			Console.WriteLine($"Restarted web GUI. ({restartResult["action"]}:{restartResult["message"]})");
+			Console.WriteLine($"Done.");
+		}
+	}
+}
---
+++ Util.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UpdatePfSenseCerts
+{
+	static class Entensions
+	{
+		public static TResult Let<T, TResult>(this T arg, Func<T, TResult> func) => func(arg);
+		public static T Also<T>(this T arg, Action<T> func) { func(arg); return arg; }
+
+		public static string ToTitleCase(this string str) =>
+			CultureInfo.InvariantCulture.TextInfo.ToTitleCase(str);
+
+		public static string ToHexString(this byte[] bytes) =>
+			BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
+
+		public static byte[] GetBytes(this string str) =>
+			Encoding.UTF8.GetBytes(str);
+
+		public static string GetString(this byte[] bytes) =>
+			Encoding.UTF8.GetString(bytes);
+
+		public static string GetBase64(this byte[] bytes) =>
+			Convert.ToBase64String(bytes);
+
+		public static string ReadAllText(this Stream stream)
+		{
+			using var reader = new StreamReader(stream, new UTF8Encoding(false), false, 4096, true);
+			return reader.ReadToEnd();
+		}
+	}
+
+	static class Util
+	{
+		public static byte[] Random(int length)
+		{
+			using var rng = new RNGCryptoServiceProvider();
+			var bytes = new byte[length];
+			rng.GetBytes(bytes);
+			return bytes;
+		}
+
+		public static byte[] Sha256(byte[] input)
+		{
+			using var sha256 = SHA256.Create();
+			return sha256.ComputeHash(input);
+		}
+	}
+}
